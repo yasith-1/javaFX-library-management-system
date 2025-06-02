2cd3b764d1cf5cd421d281bc7d45b483a47f9841
Yasith <141606159+yasith-1@users.noreply.github.com>
2025-06-02 08:48:28 +0530

    update dashboard design and addBook from design

--- src/main/java/controller/BookFormController.java
+++ src/main/java/controller/BookFormController.java
@@ -2,26 +2,44 @@ package controller;
 
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import service.ServiceFactory;
 import service.custom.impl.BookServiceImpl;
 import util.ServiceType;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 public class BookFormController implements Initializable {
     public JFXTextField txtId;
     public JFXTextField txtTile;
     public JFXTextField txtCopies;
-    public JFXComboBox comboAuthor;
     public JFXComboBox comboCategory;
-    public JFXComboBox comboStatus;
 
+    HashMap<String,String> categoryMap = new HashMap<>();
     BookServiceImpl service = ServiceFactory.getInstance().getServiceType(ServiceType.BOOK);
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         txtId.setText(service.getNextBookId());
+        categoryMap.put("Horror","C001");
+        categoryMap.put("Romance","C002");
+
+        comboCategory.getItems().addAll(categoryMap.keySet());
+    }
+
+
+    public void comboCategoryOnaction(ActionEvent actionEvent) {
+        System.out.println(comboCategory.getValue());
+    }
+
+    public void comboStatusOnaction(ActionEvent actionEvent) {
+    }
+
+
+
+    public void comboAuthorOnaction(ActionEvent actionEvent) {
     }
 }
