6ca921d3f3ad719deb9ac4b3aa7bf2c890e7a5c4
Yasith <141606159+yasith-1@users.noreply.github.com>
2025-06-15 00:42:15 +0530

    return book form combobx full fill with database data

--- src/main/java/controller/BookReturnFormController.java
+++ src/main/java/controller/BookReturnFormController.java
@@ -2,10 +2,16 @@ package controller;
 
 import com.jfoenix.controls.JFXComboBox;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import service.ServiceFactory;
+import service.custom.impl.IssuedBookServiceImpl;
+import util.ServiceType;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class BookReturnFormController {
+public class BookReturnFormController implements Initializable {
     public JFXComboBox comboBook;
     public JFXComboBox comboMember;
     public TableView returnBookTable;
@@ -14,13 +20,22 @@ public class BookReturnFormController {
     public TableColumn colBookName;
     public TableColumn colReturnTime;
 
+    IssuedBookServiceImpl service = ServiceFactory.getInstance().getServiceType(ServiceType.ISSUEDBOOK);
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadComboBoxData();
+    }
+
+    private void loadComboBoxData() {
+        comboBook.getItems().addAll(service.getBookMap().keySet());
+        comboMember.getItems().addAll(service.getMemberMap().keySet());
+    }
+
     public void addOnActionBtn(ActionEvent actionEvent) {
     }
 
     public void clearOnActionBtn(ActionEvent actionEvent) {
-    }
-
-    public void updateOnActionBtn(ActionEvent actionEvent) {
     }
 
     public void deleteOnActionBtn(ActionEvent actionEvent) {
