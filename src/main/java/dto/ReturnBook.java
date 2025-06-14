fb888d83c9e0da129296c99695db804f625f26c8
Yasith <141606159+yasith-1@users.noreply.github.com>
2025-06-14 21:35:44 +0530

    return book DTO and ENTITY attributes added

--- src/main/java/dto/ReturnBook.java
+++ src/main/java/dto/ReturnBook.java
@@ -1,4 +1,17 @@
 package dto;
 
+import lombok.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ReturnBook {
+    private String memberId;
+    private String isbn;
+    private LocalDate returnDate;
+    private LocalTime returnTime;
 }
--- src/main/java/entity/ReturnBookEntity.java
+++ src/main/java/entity/ReturnBookEntity.java
@@ -1,4 +1,17 @@
 package entity;
 
+import lombok.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ReturnBookEntity {
+    private String memberId;
+    private String isbn;
+    private LocalDate returnDate;
+    private LocalTime returnTime;
 }
