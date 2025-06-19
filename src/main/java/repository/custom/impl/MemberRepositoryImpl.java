819472eb34af9de16502d64bdc4bf4645bbee845
Yasith <141606159+yasith-1@users.noreply.github.com>
2025-06-20 00:37:02 +0530

    MemberRepositoryImpl method overieded

--- src/main/java/repository/custom/impl/MemberRepositoryImpl.java
+++ src/main/java/repository/custom/impl/MemberRepositoryImpl.java
@@ -1,6 +1,26 @@
 package repository.custom.impl;
 
+import entity.MemberEntity;
 import repository.custom.MemberRepository;
 
 public class MemberRepositoryImpl implements MemberRepository {
+    @Override
+    public Boolean add(MemberEntity entity) {
+        return null;
+    }
+
+    @Override
+    public Boolean update(MemberEntity entity) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(MemberEntity entity) {
+        return null;
+    }
+
+    @Override
+    public MemberEntity search(MemberEntity entity) {
+        return null;
+    }
 }
