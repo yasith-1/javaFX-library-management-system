be0dad1e3a210274f980f305c819ef99d803d36d
Yasith <141606159+yasith-1@users.noreply.github.com>
2025-06-21 08:34:37 +0530

    delayReturnedMembersList() method implemented

--- src/main/java/repository/custom/DelayReturnRepository.java
+++ src/main/java/repository/custom/DelayReturnRepository.java
@@ -1,6 +1,10 @@
 package repository.custom;
 
+import entity.DelayReturnEntity;
 import repository.SuperRepository;
+import java.util.List;
 
 public interface DelayReturnRepository extends SuperRepository {
+    List<DelayReturnEntity> delayReturnedMembersList();
+    List<DelayReturnEntity> delayReturnedOverviewList();
 }
--- src/main/java/repository/custom/impl/DelayReturnRepositoryImpl.java
+++ src/main/java/repository/custom/impl/DelayReturnRepositoryImpl.java
@@ -1,7 +1,44 @@
 package repository.custom.impl;
 
+import entity.DelayReturnEntity;
 import repository.custom.DelayReturnRepository;
+import util.CrudUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DelayReturnRepositoryImpl implements DelayReturnRepository {
 
+    @Override
+    public List<DelayReturnEntity> delayReturnedMembersList() {
+        ArrayList<DelayReturnEntity> delayReturnEntityMemberList = new ArrayList<>();
+        try {
+            ResultSet resultSet = CrudUtil.execute("SELECT `member_has_book`.`member_id`,`name`,DATEDIFF(`returned_date`, `return_date`) " +
+                    "AS `delayed_days` FROM `member_has_book` INNER JOIN `member` ON `member_has_book`.`member_id` = `member`.`id` " +
+                    "INNER JOIN `return_book` ON `member`.`id` = `return_book`.`member_id`");
+
+            while (resultSet.next()) {
+                if (resultSet.getInt("delayed_days") > 0) {
+                    DelayReturnEntity delayReturnEntity = new DelayReturnEntity(resultSet.getString("member_id"),
+                            null,
+                            resultSet.getString("name"),
+                            null, null, null, null, null);
+
+                    delayReturnEntityMemberList.add(delayReturnEntity);
+                }
+                continue;
+            }
+            return null;
+        } catch (SQLException e) {
+            e.getMessage();
+            return null;
+        }
+    }
+
+    @Override
+    public List<DelayReturnEntity> delayReturnedOverviewList() {
+        return List.of();
+    }
 }
