73156ce7bb0893820a6da196aea740ac45d6bdc8
Yasith <141606159+yasith-1@users.noreply.github.com>
2025-06-21 07:13:49 +0530

    Add ID s to design components

--- src/main/java/controller/DelayReturnFormController.java
+++ src/main/java/controller/DelayReturnFormController.java
@@ -1,4 +1,22 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class DelayReturnFormController {
+    public TableView membersTable;
+    public ComboBox comboMembers;
+    public TableView dellayReturnOverviewTable;
+    public TableColumn colMemberId;
+    public TableColumn colIssueDate;
+    public TableColumn colMemberName;
+    public TableColumn colDateToReturn;
+    public TableColumn colReturnedDate;
+    public TableColumn colReturnedTime;
+    public TableColumn colDelayedDays;
+
+    public void makeFineOnActionBtn(ActionEvent actionEvent) {
+    }
 }
