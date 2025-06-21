6229e74bd16f975e8a5a6eb634ab22b95c4aaba5
Yasith <141606159+yasith-1@users.noreply.github.com>
2025-06-21 08:47:59 +0530

    getDelayReturnMembersList() method implemented in service layer

--- src/main/java/service/custom/DelayReturnService.java
+++ src/main/java/service/custom/DelayReturnService.java
@@ -1,6 +1,10 @@
 package service.custom;
 
+import dto.DelayReturn;
 import service.SuperService;
 
+import java.util.List;
+
 public interface DelayReturnService extends SuperService {
+    List<DelayReturn> getDelayReturnMembersList();
 }
--- src/main/java/service/custom/impl/DelayReturnServiceImpl.java
+++ src/main/java/service/custom/impl/DelayReturnServiceImpl.java
@@ -1,4 +1,31 @@
 package service.custom.impl;
 
-public class DelayReturnServiceImpl {
+import dto.DelayReturn;
+import entity.DelayReturnEntity;
+import org.modelmapper.ModelMapper;
+import repository.RepositoryFactory;
+import repository.custom.impl.DelayReturnRepositoryImpl;
+import service.custom.DelayReturnService;
+import util.Mapper;
+import util.RepositoryType;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DelayReturnServiceImpl implements DelayReturnService {
+
+    DelayReturnRepositoryImpl repository = RepositoryFactory.getInstance().getRepositoryType(RepositoryType.DELAYEDRETURN);
+    ModelMapper modelMapper = Mapper.getInstance().getModelMapper();
+
+    @Override
+    public List<DelayReturn> getDelayReturnMembersList() {
+        ArrayList<DelayReturn> delayReturnMemberList = new ArrayList<>();
+        List<DelayReturnEntity> delayReturnMemberEntities = repository.delayReturnedMembersList();
+        if (delayReturnMemberEntities != null) {
+            delayReturnMemberEntities.forEach(delayReturnEntity -> {
+                delayReturnMemberList.add(modelMapper.map(delayReturnEntity, DelayReturn.class));
+            });
+            return delayReturnMemberList;
+        }
+        return null;
+    }
 }
