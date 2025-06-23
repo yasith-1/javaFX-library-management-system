d160880dc84d58b3dab43db48661e9ab8474f003
Yasith <141606159+yasith-1@users.noreply.github.com>
2025-06-23 15:10:54 +0530

    register and login form redirection part make sured

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -3,18 +3,28 @@ package controller;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class LoginFormController {
-    public JFXTextField usernameField;
     public JFXPasswordField passwordField;
-
-    public void signupOnActionButton(ActionEvent actionEvent) {
-    }
+    public JFXTextField txtEmailORnic;
 
     public void LoginOnActionButton(ActionEvent actionEvent) {
     }
 
-    public void redirectToLoginOnActionBtn(ActionEvent actionEvent) {
+    public void redirectToRegisterOnActionBtn(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.close();
 
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/adminSignupForm.fxml"))));
+        stage.setTitle("Register Form");
+        stage.setResizable(false);
+        stage.getIcons().add(new Image("/image/stageicon.png"));
+        stage.show();
     }
 }
--- src/main/java/controller/SignupFormController.java
+++ src/main/java/controller/SignupFormController.java
@@ -1,4 +1,34 @@
 package controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 public class SignupFormController {
+    public JFXTextField txtNic;
+    public JFXPasswordField txtPassword;
+    public JFXTextField txtName;
+    public JFXTextField txtAddress;
+    public JFXTextField txtEmail;
+
+    public void RegisterOnActionButton(ActionEvent actionEvent) {
+
+    }
+
+    public void redirectToLoginOnActionBtn(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.close();
+
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/adminLoginForm.fxml"))));
+        stage.setTitle("Login Form");
+        stage.setResizable(false);
+        stage.getIcons().add(new Image("/image/stageicon.png"));
+        stage.show();
+    }
 }
