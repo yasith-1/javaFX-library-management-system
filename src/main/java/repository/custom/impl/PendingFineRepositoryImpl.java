ea81a4e8d757b11caaee897899525fbc847d60a5
Yasith <141606159+yasith-1@users.noreply.github.com>
2025-06-23 11:43:49 +0530

    update query as see sell all pending unpaid fine data list

--- src/main/java/repository/custom/impl/PendingFineRepositoryImpl.java
+++ src/main/java/repository/custom/impl/PendingFineRepositoryImpl.java
@@ -15,13 +15,21 @@ public class PendingFineRepositoryImpl implements PendingFineRepository {
         ArrayList<PendingFineEntity> pendingFineEntityArrayList = new ArrayList<>();
         try {
             ResultSet resultSet = CrudUtil.execute("SELECT \n" +
-                    "    `member_has_book`.`member_id`,\n" +
-                    "    `member_has_book`.book_isbn,\n" +
-                    "    `name`,\n" +
-                    "    DATEDIFF(`returned_date`, `return_date`) AS `delayed_days`\n" +
-                    "FROM `member_has_book` \n" +
-                    "INNER JOIN `member` ON `member_has_book`.`member_id` = `member`.`id` \n" +
-                    "INNER JOIN `return_book` ON `member`.`id` = `return_book`.`member_id`");
+                    "    member_has_book.member_id,\n" +
+                    "    member_has_book.book_isbn,\n" +
+                    "    member.name,\n" +
+                    "    DATEDIFF(return_book.returned_date, member_has_book.return_date) AS delayed_days\n" +
+                    "FROM member_has_book\n" +
+                    "INNER JOIN member \n" +
+                    "    ON member_has_book.member_id = member.id\n" +
+                    "INNER JOIN return_book \n" +
+                    "    ON member.id = return_book.member_id\n" +
+                    "INNER JOIN fine \n" +
+                    "    ON member_has_book.member_id = fine.member_id \n" +
+                    "    AND member_has_book.book_isbn = fine.book_isbn\n" +
+                    "INNER JOIN fine_status \n" +
+                    "    ON fine.fine_status_id = fine_status.id\n" +
+                    "WHERE fine_status.status =?","unpaid");
 
             while (resultSet.next()) {
                 if (resultSet.getInt("delayed_days") > 0) {
