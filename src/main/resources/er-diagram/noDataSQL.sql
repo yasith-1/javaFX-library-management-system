9732355b8d199d63f9e6819c5b97a012294b27ec
Yasith <141606159+yasith-1@users.noreply.github.com>
2025-06-27 13:19:51 +0530

    refactor: rename lms_system_backup.sql to noDataSQL.sql and remove data export statements

--- src/main/resources/er-diagram/noDataSQL.sql
+++ src/main/resources/er-diagram/noDataSQL.sql
@@ -26,12 +26,7 @@ CREATE TABLE IF NOT EXISTS `author` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table lms_system.author: ~4 rows (approximately)
-INSERT INTO `author` (`id`, `name`) VALUES
-	('A001', 'Martin Wikramasingha'),
-	('A002', 'Author c Clark'),
-	('A003', 'Mahagama Sekara'),
-	('A004', 'Arisen Ahubudu');
+-- Data exporting was unselected.
 
 -- Dumping structure for table lms_system.book
 CREATE TABLE IF NOT EXISTS `book` (
@@ -50,10 +45,7 @@ CREATE TABLE IF NOT EXISTS `book` (
   CONSTRAINT `fk_book_status1` FOREIGN KEY (`status_id`) REFERENCES `book_status` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table lms_system.book: ~0 rows (approximately)
-INSERT INTO `book` (`isbn`, `title`, `copies`, `status_id`, `gerne_id`, `author_id`) VALUES
-	('B001', 'Madolduwa', 11, 'S001', 'G001', 'A001'),
-	('B002', 'Gamperaliya', 24, 'S001', 'G001', 'A001');
+-- Data exporting was unselected.
 
 -- Dumping structure for table lms_system.book_status
 CREATE TABLE IF NOT EXISTS `book_status` (
@@ -62,10 +54,7 @@ CREATE TABLE IF NOT EXISTS `book_status` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table lms_system.book_status: ~2 rows (approximately)
-INSERT INTO `book_status` (`id`, `status`) VALUES
-	('S001', 'Available'),
-	('S002', 'Unavailable');
+-- Data exporting was unselected.
 
 -- Dumping structure for table lms_system.fine
 CREATE TABLE IF NOT EXISTS `fine` (
@@ -85,10 +74,7 @@ CREATE TABLE IF NOT EXISTS `fine` (
   CONSTRAINT `fk_fine_fine_status1` FOREIGN KEY (`fine_status_id`) REFERENCES `fine_status` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table lms_system.fine: ~1 rows (approximately)
-INSERT INTO `fine` (`id`, `reason`, `paid_date`, `paid_time`, `amount`, `member_id`, `book_isbn`, `fine_status_id`) VALUES
-	('F001', 'missed to return', '2025-06-21', '18:32:47', 1200, 'M002', 'B002', 'FS002'),
-	('F002', 'messed up return date', '2025-06-22', '20:49:15', 400, 'M001', 'B001', 'FS002');
+-- Data exporting was unselected.
 
 -- Dumping structure for table lms_system.fine_status
 CREATE TABLE IF NOT EXISTS `fine_status` (
@@ -97,10 +83,7 @@ CREATE TABLE IF NOT EXISTS `fine_status` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table lms_system.fine_status: ~2 rows (approximately)
-INSERT INTO `fine_status` (`id`, `status`) VALUES
-	('FS001', 'Paid'),
-	('FS002', 'Unpaid');
+-- Data exporting was unselected.
 
 -- Dumping structure for table lms_system.gerne
 CREATE TABLE IF NOT EXISTS `gerne` (
@@ -109,14 +92,7 @@ CREATE TABLE IF NOT EXISTS `gerne` (
   PRIMARY KEY (`gerne_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table lms_system.gerne: ~6 rows (approximately)
-INSERT INTO `gerne` (`gerne_id`, `name`) VALUES
-	('G001', 'Sinhala'),
-	('G002', 'English'),
-	('G003', 'Horror'),
-	('G004', 'History'),
-	('G005', 'Bilogy'),
-	('G006', 'Mith');
+-- Data exporting was unselected.
 
 -- Dumping structure for table lms_system.member
 CREATE TABLE IF NOT EXISTS `member` (
@@ -125,16 +101,14 @@ CREATE TABLE IF NOT EXISTS `member` (
   `nic` varchar(15) DEFAULT NULL,
   `email` varchar(45) NOT NULL,
   `address` varchar(45) NOT NULL,
-  `password` varchar(20) DEFAULT NULL,
+  `password` varchar(80) DEFAULT NULL,
   `type_id` varchar(5) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_member_member_type1_idx` (`type_id`)
+  KEY `fk_member_member_type1_idx1` (`type_id`),
+  CONSTRAINT `fk_member_member_type1` FOREIGN KEY (`type_id`) REFERENCES `member_type` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table lms_system.member: ~2 rows (approximately)
-INSERT INTO `member` (`id`, `name`, `nic`, `email`, `address`, `password`, `type_id`) VALUES
-	('M001', 'Yasith prabashwara', '200225700817', 'yasith@gmail.com', 'kelaniya', '(NULL)', 'T2'),
-	('M002', 'Taniya Anurangi', '200225700816', 'tanya@gmail.com', 'wattala', NULL, 'T2');
+-- Data exporting was unselected.
 
 -- Dumping structure for table lms_system.member_has_book
 CREATE TABLE IF NOT EXISTS `member_has_book` (
@@ -151,23 +125,16 @@ CREATE TABLE IF NOT EXISTS `member_has_book` (
   CONSTRAINT `fk_member_has_book_member1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table lms_system.member_has_book: ~3 rows (approximately)
-INSERT INTO `member_has_book` (`member_id`, `book_isbn`, `issue_qty`, `issue_date`, `issue_time`, `return_date`) VALUES
-	('M001', 'B001', 1, '2025-06-22', '18:52:58', '2025-06-25'),
-	('M001', 'B002', 1, '2025-06-22', '22:54:27', '2025-06-25'),
-	('M002', 'B002', 1, '2025-06-21', '18:25:14', '2025-06-24');
+-- Data exporting was unselected.
 
 -- Dumping structure for table lms_system.member_type
 CREATE TABLE IF NOT EXISTS `member_type` (
-  `id` varchar(50) NOT NULL DEFAULT '',
+  `id` varchar(5) NOT NULL DEFAULT '',
   `type` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table lms_system.member_type: ~2 rows (approximately)
-INSERT INTO `member_type` (`id`, `type`) VALUES
-	('T1', 'Admin'),
-	('T2', 'Member');
+-- Data exporting was unselected.
 
 -- Dumping structure for table lms_system.return_book
 CREATE TABLE IF NOT EXISTS `return_book` (
@@ -181,10 +148,7 @@ CREATE TABLE IF NOT EXISTS `return_book` (
   CONSTRAINT `fk_return_book_member1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table lms_system.return_book: ~2 rows (approximately)
-INSERT INTO `return_book` (`member_id`, `book_isbn`, `returned_date`, `returned_time`) VALUES
-	('M001', 'B001', '2025-06-26', '18:53:54'),
-	('M002', 'B002', '2025-06-27', '18:29:56');
+-- Data exporting was unselected.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
